2884ee7f2439a5056003f618499715d7b269e6ab
messixing <519967536@qq.com>
2018-05-05 11:35:04 +0800

    Site updated: 2018-05-05 11:35:03

---
+++ thz-wap/js/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: xingwei
+ * @Date:   2018-05-04 10:28:15
+ * @Last Modified by:   xingwei
+ * @Last Modified time: 2018-05-05 11:04:19
+ */
+jQuery(document).ready(function($) {
+	$('.demo2').Tabs({
+
+		event: 'click'
+
+	});
+	$('.demo3').Tabs({
+
+		event: 'click'
+
+	});
+	$('.demo4').Tabs({
+
+		event: 'click'
+
+	});
+});
